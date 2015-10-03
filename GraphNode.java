e763be34779bad5a722dc500de29d8b12d6d74e2
adambenn <adambenn22@gmail.com>
2015-10-03 14:55:06 -0400

    GraphNode added

---
+++ GraphNode.java
@@ -0,0 +1,15 @@
+public class GraphNode {
+	private int lastSet = 0; // Turn that dangerous was last set
+	
+	public void setDangerous(int currentTurn){
+		lastSet = currentTurn;
+	}
+	
+	public boolean isDangerous(int currentTurn){
+		if( currentTurn == lastSet){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
